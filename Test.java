45ecdc46e9f16250c4c7eb9813889b8dbd950485
unknown <biradargoudarvani@gmail.com>
2022-10-03 21:24:47 +0530

    firstcommit

---
+++ Test.java
@@ -0,0 +1,16 @@
+package MyPackage;
+
+public class Test {
+     //main driver method 
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println();
+		double a=12.5;
+		int b=(int)a;
+		System.out.println(a+" and " +b);
+
+	}
+
+}
